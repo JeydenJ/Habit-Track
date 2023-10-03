0a9a1b05c6ef44f505a469b4faf5cd08b6a6d4b7
Jeyden Johnson <Jeyden.Johnson-1@student.uts.edu.au>
2023-10-03 20:33:33 +1100

    Initial Commit

---
+++ Habit Track/Habit_TrackApp.swift
@@ -0,0 +1,20 @@
+//
+//  Habit_TrackApp.swift
+//  Habit Track
+//
+//  Created by Jeyden Johnson on 3/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
